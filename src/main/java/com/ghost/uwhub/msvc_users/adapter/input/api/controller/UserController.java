94e34b142bde43d795519a63852bd617e698e79e
Alejandro Giraldo Serna <giraldosernaalejandro16@gmail.com>
2025-04-17 09:09:28 -0500

    Swagger was added

--- src/main/java/com/ghost/uwhub/msvc_users/adapter/input/api/controller/UserController.java
+++ src/main/java/com/ghost/uwhub/msvc_users/adapter/input/api/controller/UserController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
-@RequestMapping(path = "")
+@RequestMapping(path = "/users")
 public class UserController {
 
     @Autowired
---
+++ src/main/java/com/ghost/uwhub/msvc_users/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,12 @@
+package com.ghost.uwhub.msvc_users.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "UWH API", version = "v1", description = "Documentación de la API REST UWH user"))
+public class OpenApiConfig {
+    
+}
